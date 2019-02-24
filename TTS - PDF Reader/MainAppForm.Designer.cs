f9a4b85d26eb3d454c0c02e79ffc52a27b56175e
OBSDARK <jjohnen01@gmail.com>
2019-02-24 18:33:43 -0300

    Agregar archivos de proyecto.

---
+++ TTS - PDF Reader/MainAppForm.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace TTS___PDF_Reader
+{
+    partial class MainAppForm
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbxText = new System.Windows.Forms.RichTextBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnSpeak = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnResume = new System.Windows.Forms.Button();
+            this.btnEpub = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rtbxText
+            // 
+            this.rtbxText.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtbxText.Location = new System.Drawing.Point(12, 12);
+            this.rtbxText.Name = "rtbxText";
+            this.rtbxText.Size = new System.Drawing.Size(776, 397);
+            this.rtbxText.TabIndex = 0;
+            this.rtbxText.Text = "";
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOpen.Location = new System.Drawing.Point(713, 415);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "Abrir PDF";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnSpeak
+            // 
+            this.btnSpeak.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnSpeak.Location = new System.Drawing.Point(233, 415);
+            this.btnSpeak.Name = "btnSpeak";
+            this.btnSpeak.Size = new System.Drawing.Size(75, 23);
+            this.btnSpeak.TabIndex = 2;
+            this.btnSpeak.Text = "Comenzar";
+            this.btnSpeak.UseVisualStyleBackColor = true;
+            this.btnSpeak.Click += new System.EventHandler(this.btnSpeak_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnPause.Location = new System.Drawing.Point(422, 415);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(75, 23);
+            this.btnPause.TabIndex = 3;
+            this.btnPause.Text = "Pausar";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnStop.Location = new System.Drawing.Point(518, 415);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 4;
+            this.btnStop.Text = "Detener";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnResume
+            // 
+            this.btnResume.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnResume.Location = new System.Drawing.Point(327, 415);
+            this.btnResume.Name = "btnResume";
+            this.btnResume.Size = new System.Drawing.Size(75, 23);
+            this.btnResume.TabIndex = 5;
+            this.btnResume.Text = "Continuar";
+            this.btnResume.UseVisualStyleBackColor = true;
+            this.btnResume.Click += new System.EventHandler(this.btnResume_Click);
+            // 
+            // btnEpub
+            // 
+            this.btnEpub.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnEpub.Location = new System.Drawing.Point(632, 415);
+            this.btnEpub.Name = "btnEpub";
+            this.btnEpub.Size = new System.Drawing.Size(75, 23);
+            this.btnEpub.TabIndex = 6;
+            this.btnEpub.Text = "Abrir EPUB";
+            this.btnEpub.UseVisualStyleBackColor = true;
+            this.btnEpub.Click += new System.EventHandler(this.btnEpub_Click);
+            // 
+            // MainAppForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnEpub);
+            this.Controls.Add(this.btnResume);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.btnSpeak);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.rtbxText);
+            this.Name = "MainAppForm";
+            this.Text = "TTS - PDF and EPUB Starwars Reader";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbxText;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnSpeak;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnResume;
+        private System.Windows.Forms.Button btnEpub;
+    }
+}
+
---
+++ TTS - PDF Reader/MainAppForm.cs
@@ -0,0 +1,172 @@
+﻿using iTextSharp.text.pdf.parser;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VersOne.Epub;
+using HtmlAgilityPack;
+using HtmlDocument = HtmlAgilityPack.HtmlDocument;
+
+namespace TTS___PDF_Reader
+{
+    public partial class MainAppForm : Form
+    {
+        public MainAppForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "PDF files|*.pdf", ValidateNames = true, Multiselect = false })
+            {
+                if(ofd.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        iTextSharp.text.pdf.PdfReader reader = new iTextSharp.text.pdf.PdfReader(ofd.FileName);
+                        StringBuilder sb = new StringBuilder();
+                        for (int i = 1; i <= reader.NumberOfPages; i++)
+                        {
+                            sb.Append(PdfTextExtractor.GetTextFromPage(reader, i));
+                        }
+                        rtbxText.Text = sb.ToString();
+                        reader.Close();
+                    }
+                    catch(Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+
+        private void btnEpub_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "EPUB files|*.epub", ValidateNames = true, Multiselect = false })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        EpubBook ebook = EpubReader.ReadBook(ofd.FileName);
+                        StringBuilder sb = new StringBuilder();
+                        foreach (EpubChapter chapter in ebook.Chapters)
+                        {
+                            sb.Append(MainAppForm.PrintChapter(chapter));
+                        }
+                        rtbxText.Text = sb.ToString();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private static string PrintChapter(EpubChapter chapter)
+        {
+            HtmlDocument htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(chapter.HtmlContent);
+            StringBuilder sb = new StringBuilder();
+            foreach (HtmlNode node in htmlDocument.DocumentNode.SelectNodes("//text()"))
+            {
+                sb.AppendLine(node.InnerText.Trim());
+            }
+            return sb.ToString();
+
+            /*string chapterTitle = chapter.Title;
+            string chapterText = sb.ToString();
+            Console.WriteLine("------------ ", chapterTitle, "------------ ");
+            Console.WriteLine(chapterText);
+            Console.WriteLine();
+            foreach (EpubChapter subChapter in chapter.SubChapters)
+            {
+                PrintChapter(subChapter);
+            }*/
+        }
+
+        SpeechSynthesizer speech = new SpeechSynthesizer();
+
+        private void btnSpeak_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrWhiteSpace(rtbxText.Text))
+                {
+                    speech.Dispose();
+                    speech = new SpeechSynthesizer();
+                    speech.SpeakAsync(rtbxText.Text);
+                }
+                else
+                {
+                    MessageBox.Show("Ningun texto ha sido cargado, por favor seleccione uno e intente nuevamente");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (speech != null)
+                {
+                    if (speech.State == SynthesizerState.Speaking)
+                    {
+                        speech.Pause();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnResume_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (speech != null)
+                {
+                    if (speech.State == SynthesizerState.Paused)
+                    {
+                        speech.Resume();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (speech != null)
+                {
+                    speech.Dispose();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+    }
+}
